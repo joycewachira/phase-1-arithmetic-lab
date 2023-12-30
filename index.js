a2a017a21c966a724a7bef6099c0d8a5c0e28ec9
Joyce <joycewachira718@gmail.com>
2023-12-30 18:06:48 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let multiply;
+let num1 = 31;
+let num2 =2;
 
-// Write your code here
+multiply =num1 *num2;
+
+let random=Math.floor(Math.random() * 10) + 1;
+console.log(random)
+
+let max  =Math.max(1,2,5,6,14,5,7,20)
+
+
+let num3=24 ;
+let num4 =5;
+let mod =num3 % num4;
